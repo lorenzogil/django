42056edad6aea72f664ee35023c8397841dec664
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-09-11 13:29:14 +0200

    Add a new test to show the empty fixtures problem
    When a test defines and empty list as its fixtures attribute
    an error is thrown because the command loaddata does not know
    how to handle this situation.

--- tests/fixtures/tests.py
+++ tests/fixtures/tests.py
@@ -24,6 +24,14 @@ class TestCaseFixtureLoadingTests(TestCase):
         ])
 
 
+class SubclassTestCaseFixtureLoadingTests(TestCaseFixtureLoadingTests):
+    fixtures = []
+
+    def testClassFixtures(self):
+        "Check that there were no fixture objects installed"
+        self.assertEqual(Article.objects.count(), 0)
+
+
 class DumpDataAssertMixin(object):
 
     def _dumpdata_assert(self, args, output, format='json', natural_keys=False,
